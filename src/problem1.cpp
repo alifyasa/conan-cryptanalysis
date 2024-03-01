bca372d546e9405db5633bcf95b2211cb85f74eb
FOS-Intern <intern@fata-organa.com>
2024-03-01 13:26:13 +0700

    Add unigram,bigram,trigram counting for each word by counting sort

--- src/problem1.cpp
+++ src/problem1.cpp
@@ -5,7 +5,25 @@ using namespace std;
 
 typedef long long ll;
 
-ll arr[36];
+struct gramItem {
+    string gram;
+    int count;
+};
+
+struct trigramPost{
+    int first;
+    int second;
+    int third;
+};
+
+struct bigramPost{
+    int first;
+    int second;
+};
+
+ll arrUnigram[36], arrBigram[36][36], arrTrigram[36][36][36];
+ll countValueUnigram[36], countValueBigram[36*36], countValueTrigram[36*36*36];
+char firstChar,secondChar;
 int convertHashFromChar(char c){
     int temp = (int)c;
     if(temp >= 65) return temp-65;
@@ -18,20 +36,54 @@ char convertHashFromInt(int n){
 void solve(){
     freopen("../test/input/problem1.txt", "r", stdin);
     freopen("../test/output/problem1.txt", "w", stdout);
-    int count = 0;
-    string s; s = "";
     string temp;
+    string tempLast2 = "XX";
+    int lengthPlainText = 0;
     // Keep reading lines until an empty line is encountered
-    while (getline(cin, temp)&&!temp.empty()) {
-        s += temp;
+    int countDebug=0;
+    while(getline(cin, temp)&&!temp.empty()) {
+        countDebug++;
+        ll tempLengthTemp = temp.length();
+        for(int i = 0; i < tempLengthTemp; i++){
+            arrUnigram[convertHashFromChar(temp[i])]++;
+            if(lengthPlainText%2==1){
+                firstChar = tempLast2[1];
+                if(i>0) firstChar = temp[i-1];
+                arrBigram[convertHashFromChar(firstChar)][convertHashFromChar(temp[i])]++;
+            }
+            if(lengthPlainText%3==2){
+                firstChar = tempLast2[0];
+                secondChar = tempLast2[1];
+                if(i>1) firstChar = temp[i-2];
+                if(i>0) secondChar = temp[i-1];
+                arrTrigram[convertHashFromChar(firstChar)][convertHashFromChar(secondChar)][convertHashFromChar(temp[i])]++;
+            }
+            lengthPlainText++;
+        }
+        tempLast2 = "";
+        tempLast2 += temp[tempLengthTemp-2];
+        tempLast2 += temp[tempLengthTemp-1];
     }
-    ll szS = s.length();
 
-    // Do frequency count first for each character on string
-    for(ll i = 0; i < szS; i++){
-        arr[convertHashFromChar(s[i])]++;
-    }
-    cout << s << "\n";
+    // Using counting sort to sorting the frequency of unigram, bigram, and trigram
+    ll countValueUnigram[lengthPlainText+1];
+    ll countValueBigram[lengthPlainText];
+    ll countValueTrigram[lengthPlainText-1];
+    for(int i = 0; i<=lengthPlainText; i++) countValueUnigram[i] = 0;
+    for(int i = 0; i<lengthPlainText; i++) countValueBigram[i] = 0;
+    for(int i = 0; i<lengthPlainText-1; i++) countValueTrigram[i] = 0;
+    // Count each value of unigram, bigram, and trigram
+    for(int i=0; i<36; i++) if(arrUnigram[i]!=0)countValueUnigram[arrUnigram[i]]++;
+    for(int i=0; i<36; i++) for(int j=0; j<36; j++) if(arrBigram[i][j]!=0) countValueBigram[arrBigram[i][j]]++;
+    for(int i=0; i<36; i++) for(int j=0; j<36; j++) for(int k=0; k<36; k++) if(arrTrigram[i][j][k]!=0) countValueTrigram[arrTrigram[i][j][k]]++;
+    // Count cumulative value of unigram, bigram, and trigram using dynamic programming
+    for(int i=0; i<36; i++) countValueUnigram[arrUnigram[i]] += countValueUnigram[arrUnigram[i]-1];
+    for(int i=0; i<36; i++) for(int j=0; j<36; j++)countValueBigram[arrBigram[i][j]] += countValueBigram[arrBigram[i][j]-1];
+    for(int i=0; i<36; i++) for(int j=0; j<36; j++) for(int k=0; k<36; k++) countValueTrigram[arrTrigram[i][j][k]] += countValueTrigram[arrTrigram[i][j][k]-1];
+
+    // Bigram Analysis
+
+    // Trigram Analysis
 
     // We can rank the frequency from the highest to the lowest with bigram and trigram list
 
@@ -39,7 +91,6 @@ void solve(){
 }
  
 int main() {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
  
     solve();
  
