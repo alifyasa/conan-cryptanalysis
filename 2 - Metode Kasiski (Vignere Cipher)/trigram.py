ea49579ff2332ac117c3e1d09dec8eb5c647369c
FOS-Intern <intern@fata-organa.com>
2024-03-03 17:49:40 +0700

    add trigram distance calculator

--- 2 - Metode Kasiski (Vignere Cipher)/trigram.py
+++ 2 - Metode Kasiski (Vignere Cipher)/trigram.py
@@ -10,6 +10,19 @@ def get_all_repeated_trigram(ciphertext):
     ret_trigram_pos = [(trigram, trigram_pos[trigram]) for trigram in repeated_list]
     return ret_trigram_pos
 
+def get_all_each_distance(repeated_trigrams):
+    distances_dict = {}
+    for item in repeated_trigrams:
+        key = item[0]
+        array = item[1]
+        distances = []
+        for i in range(len(array) - 1):
+            distance = array[i + 1] - array[i]
+            distances.append(distance)
+        distances_dict[key] = distances
+    distances_list = [(key, value) for key, value in distances_dict.items()]
+    return distances_list
+
 def load_ciphertext(ciphertext_file = "ciphertext.txt"):
     with open(ciphertext_file, "r") as f:
         return ''.join(f.read().split())
@@ -27,4 +40,6 @@ if __name__ == "__main__":
     text = load_ciphertext()
     temp_all_repeated_trigram = get_all_repeated_trigram(text)
     sorted_data = sorted(temp_all_repeated_trigram, key=lambda x: len(x[1]), reverse=True)
+    temp_trigram_distance = get_all_each_distance(sorted_data)
     save_list_to_txt("repeated_trigram.txt", sorted_data)
+    save_list_to_txt("each_trigram_distance.txt", temp_trigram_distance)
